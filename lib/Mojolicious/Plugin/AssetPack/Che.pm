a37841911354c5a47744b5c26e9288a395814331
Mikhail Che <ernesto.chege@yandex.ru>
2017-01-10 15:47:23 +0500

    combine file

--- lib/Mojolicious/Plugin/AssetPack/Che.pm
+++ lib/Mojolicious/Plugin/AssetPack/Che.pm
@@ -1,12 +1,46 @@
 package Mojolicious::Plugin::AssetPack::Che;
 use Mojo::Base 'Mojolicious::Plugin::AssetPack';
+use Mojolicious::Plugin::AssetPack::Util qw( checksum );
+use Mojo::URL;
 
 sub register {
   my ($self, $app, $config) = @_;
   $self->SUPER::register($app, $config);
-  $self->process(ref eq 'ARRAY' ? @$_ : $_)
-    for ref $config->{process} eq 'HASH' ? map([$_=> @{$config->{process}{$_}}], keys %{$config->{process}}) : ref $config->{process} eq 'ARRAY' ? @{$config->{process}} : ();
+  
+  my $process = $config->{process};
+  $self->process(ref eq 'ARRAY' ? @$_ : $_) #($_->[0], map Mojo::URL->new($_), @$_[1..$#$_])
+    for ref $process eq 'HASH' ? map([$_=> @{$process->{$_}}], keys %$process) : ref $process eq 'ARRAY' ? @$process : ();
+  
+  
+  
+  
+  #~ $self->store->_types->type('html', ['text/html;charset=UTF-8']);
+    #~ if $html;
+  #~ my $html = $config->{combine_html};
+  #~ $self->combine_html(ref eq 'ARRAY' ? @$_ : $_)
+    #~ for ref $html eq 'HASH' ? map([$_=> @{$html->{$_}}], keys %$html) : ref $html eq 'ARRAY' ? @$html : ();
+  
+  #~ $app->routes->route('/assets/*topic')->via(qw(HEAD GET))
+    #~ ->name('assetpack by topic')->to(cb => $self->_cb_serve_by_topic);
+  
   return $self;
+}
+
+#~ sub combine_html {
+  #~ my ($self, $topic, @input) = @_;
+  #~ my $assets = Mojo::Collection->new();    # Do not mess up input
+  
+  #~ push @$assets, $self->store->asset($_)
+    #~ for @input;
+  
+  #~ my $content = $assets->map(sub { $_->content(sprintf("@@@ %s\n%s", $_->url, $_->content)) } )->map('content')->join;
+  
+  #~ $self->store->save(\$content, {key => 'combine-html', url=>$topic, name=>$topic, checksum=>checksum($topic), minified=>0, format=>'html',});#
+#~ }
+
+sub Mojo::URL::checksum {
+  1;
+  
 }
 
 =pod
@@ -25,11 +59,11 @@ Since version 1.28.
 
 =head1 VERSION
 
-Version 1.28
+Version 1.33
 
 =cut
 
-our $VERSION = '1.28';
+our $VERSION = '1.33';
 
 
 =head1 SYNOPSIS
@@ -69,3 +103,50 @@ it under the same terms as Perl itself.
 =cut
 
 1; # End of Mojolicious::Plugin::AssetPack::Che
+
+
+
+__END__
+package Mojolicious::Plugin::AssetPack::Pipe::CombineHTML;
+use Mojo::Base 'Mojolicious::Plugin::AssetPack::Pipe';
+#~ use Mojolicious::Plugin::AssetPack::Util qw(diag load_module DEBUG);
+ 
+sub process {
+  my ($self, $assets) = @_;
+  my $store = $self->assetpack->store;
+  my $file;
+  
+  my $topic = $self->topic;
+  my $format = $self->_format($topic);
+  return
+    if $format ne 'html';# or $asset->minified;
+  my $content = $assets->map(sub { $_->content(sprintf("@@@ %s\n%s", $_->url, $_->content)) } )->map('content')->join;
+  #~ return unless $self->assetpack->minify;
+  return $assets->each(
+    sub {
+      my ($asset, $index) = @_;
+      my $attrs = $asset->TO_JSON;
+      $attrs->{key}      = 'combine-html';
+      $attrs->{minified} = 0;
+      
+      return $asset->content($file)->minified(1)
+        if $file = $store->load($attrs);
+      load_module 'CSS::Minifier::XS' or die qq(Could not load "CSS::Minifier::XS": $@);
+      diag 'Minify "%s" with checksum %s.', $asset->url, $asset->checksum if DEBUG;
+      my $css = CSS::Minifier::XS::minify($asset->content);
+      $asset->content($store->save(\$css, $attrs))->minified(0);
+    }
+  );
+}
+
+sub _format {
+  my ($self, $url) = shift;
+  my $name
+    = $url =~ /^https?:/
+    ? Mojo::URL->new($url)->path->[-1]
+    : (split m!(\\|/)!, $url)[-1];
+ 
+  return $name =~ /\.(\w+)$/ ? lc $1 : '';
+};
+ 
+1;
---
+++ lib/Mojolicious/Plugin/AssetPack/Pipe/CombineFile.pm
@@ -0,0 +1,82 @@
+package Mojolicious::Plugin::AssetPack::Pipe::CombineFile;
+use Mojo::Base 'Mojolicious::Plugin::AssetPack::Pipe';
+use Mojolicious::Plugin::AssetPack::Util qw(checksum diag DEBUG);
+ 
+has enabled => sub { shift->assetpack->minify };
+
+sub new {
+  my $self = shift->SUPER::new(@_);
+  $self->assetpack->store->_types->type('html', ['text/html;charset=UTF-8']);# Restore deleted Jan
+  $self->assetpack->_app->routes->route('/assets/*topic')->via(qw(HEAD GET))
+    ->name('assetpack by topic')->to(cb => $self->_cb_route_by_topic);
+  $self;
+}
+
+sub process {
+  my ($self, $assets) = @_;
+  my $combine = Mojo::Collection->new;
+  my @other;
+  my $topic = $self->topic;
+ 
+  return unless $self->enabled;
+ 
+  for my $asset (@$assets) {
+    if ($asset->isa('Mojolicious::Plugin::AssetPack::Asset::Null')) {
+      next;
+    }
+    elsif (grep { $asset->format eq $_ } qw(css js html)) {
+      push @$combine, $asset;
+    }
+    else {
+      push @other, $asset;
+    }
+  }
+  
+  @$assets = ();
+  
+  if (@$combine) {
+    my $format = $combine->[0]->format;
+    my $checksum = checksum $topic;#$combine->map('url')->join(':');
+    my $name = checksum $topic;
+    $combine->map(sub { $_->content(sprintf("@@@ %s\n%s", $_->url, $_->content)) } )
+      if $format eq 'html';
+    my $content = $combine->map('content')->map(sub { /\n$/ ? $_ : "$_\n" })->join;
+   
+    diag 'Combining assets into "%s" with checksum[%s] and format[%s].', $topic, $checksum, $format
+      if DEBUG;
+    
+    push @$assets,
+      $self->assetpack->store->save(\$content, {key => "combine-file", url=>$topic, name=>$name, checksum=>$checksum, minified=>1, format=>$format,})
+  }
+  
+  push @$assets, @other;# preserve assets such as images and font files
+}
+
+sub _cb_route_by_topic {
+  my $assetpack  =shift->assetpack;
+return sub {
+  my $c  = shift;
+  my $topic = $c->stash('topic');
+  
+   my $assets = $assetpack->processed($topic)
+    or $c->render(text => "// The asset [$topic] does not exists or not found\n", status => 404)
+    and return;
+
+  #~ return $c->render(text => $assets->map('content')->join);
+  my $format = $assets->[0]->format;
+  my $checksum = checksum $topic;#assets->map('checksum')->join(':');
+  my $name = checksum $topic;
+  
+  my $asset = $assetpack->store->load({key => "combine-file", url=>$topic, name=>$name, checksum => $checksum, minified=>1, format=>$format});#  $format eq 'html' ? 0 : 1
+  
+  #~ warn $c->dumper($asset);#->format('tmpl')
+  
+  $assetpack->store->serve_asset($c, $asset)
+    and return $c->rendered
+    if $asset;
+ 
+  $c->render(text => "// No such asset [$topic]\n", status => 404);
+};
+}
+ 
+1;
