e53a803707c45252feda10829c91bb8542d1ed45
Mikhail Che <ernesto.chege@yandex.ru>
2017-01-11 13:18:46 +0500

    html pipe

--- lib/Mojolicious/Plugin/AssetPack/Che.pm
+++ lib/Mojolicious/Plugin/AssetPack/Che.pm
@@ -9,6 +9,7 @@ sub register {
   my ($self, $app, $config) = @_;
   $self->config($config);
   $self->app($app);
+  Scalar::Util::weaken($self->{app});
   $self->SUPER::register($app, $config);
   
   my $process = $config->{process};
@@ -16,36 +17,12 @@ sub register {
     for ref $process eq 'HASH' ? map([$_=> ref $process->{$_} eq 'ARRAY' ? @{$process->{$_}} : $process->{$_}], keys %$process) : ref $process eq 'ARRAY' ? @$process : ();
   
   
+  $app->helper(assetpack => $self);
   
-  
-  #~ $self->store->_types->type('html', ['text/html;charset=UTF-8']);
-    #~ if $html;
-  #~ my $html = $config->{combine_html};
-  #~ $self->combine_html(ref eq 'ARRAY' ? @$_ : $_)
-    #~ for ref $html eq 'HASH' ? map([$_=> @{$html->{$_}}], keys %$html) : ref $html eq 'ARRAY' ? @$html : ();
-  
-  #~ $app->routes->route('/assets/*topic')->via(qw(HEAD GET))
-    #~ ->name('assetpack by topic')->to(cb => $self->_cb_serve_by_topic);
   
   return $self;
 }
 
-#~ sub combine_html {
-  #~ my ($self, $topic, @input) = @_;
-  #~ my $assets = Mojo::Collection->new();    # Do not mess up input
-  
-  #~ push @$assets, $self->store->asset($_)
-    #~ for @input;
-  
-  #~ my $content = $assets->map(sub { $_->content(sprintf("@@@ %s\n%s", $_->url, $_->content)) } )->map('content')->join;
-  
-  #~ $self->store->save(\$content, {key => 'combine-html', url=>$topic, name=>$topic, checksum=>checksum($topic), minified=>0, format=>'html',});#
-#~ }
-
-sub Mojo::URL::checksum {
-  1;
-  
-}
 
 =pod
 
@@ -108,49 +85,3 @@ it under the same terms as Perl itself.
 
 1; # End of Mojolicious::Plugin::AssetPack::Che
 
-
-
-__END__
-package Mojolicious::Plugin::AssetPack::Pipe::CombineHTML;
-use Mojo::Base 'Mojolicious::Plugin::AssetPack::Pipe';
-#~ use Mojolicious::Plugin::AssetPack::Util qw(diag load_module DEBUG);
- 
-sub process {
-  my ($self, $assets) = @_;
-  my $store = $self->assetpack->store;
-  my $file;
-  
-  my $topic = $self->topic;
-  my $format = $self->_format($topic);
-  return
-    if $format ne 'html';# or $asset->minified;
-  my $content = $assets->map(sub { $_->content(sprintf("@@@ %s\n%s", $_->url, $_->content)) } )->map('content')->join;
-  #~ return unless $self->assetpack->minify;
-  return $assets->each(
-    sub {
-      my ($asset, $index) = @_;
-      my $attrs = $asset->TO_JSON;
-      $attrs->{key}      = 'combine-html';
-      $attrs->{minified} = 0;
-      
-      return $asset->content($file)->minified(1)
-        if $file = $store->load($attrs);
-      load_module 'CSS::Minifier::XS' or die qq(Could not load "CSS::Minifier::XS": $@);
-      diag 'Minify "%s" with checksum %s.', $asset->url, $asset->checksum if DEBUG;
-      my $css = CSS::Minifier::XS::minify($asset->content);
-      $asset->content($store->save(\$css, $attrs))->minified(0);
-    }
-  );
-}
-
-sub _format {
-  my ($self, $url) = shift;
-  my $name
-    = $url =~ /^https?:/
-    ? Mojo::URL->new($url)->path->[-1]
-    : (split m!(\\|/)!, $url)[-1];
- 
-  return $name =~ /\.(\w+)$/ ? lc $1 : '';
-};
- 
-1;
